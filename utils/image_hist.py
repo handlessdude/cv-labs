9c9155e1294aa54724578f61d84dee5973526813
heyra <zalomnova.eliz@gmail.com>
2023-09-30 18:09:23 +0300

    refactor: move color corrections to utils

--- utils/image_hist.py
+++ utils/image_hist.py
@@ -16,6 +16,13 @@ def describe_channels(img: np.ndarray) -> (np.ndarray, np.ndarray, np.ndarray):
     return r_intensities, g_intensities, b_intensities
 
 
+@njit
+def normalized_hists(img_in: np.ndarray) -> (np.ndarray, np.ndarray, np.ndarray):
+    r_hist, g_hist, b_hist = describe_channels(img_in)
+    k = img_in.shape[0] * img_in.shape[1]  # for every hist: sum of bins = k
+    return r_hist / k, g_hist / k, b_hist / k
+
+
 def plot_channel_hists(img: np.ndarray, dir_out: str, filename_out: str):
     r_intensities, g_intensities, b_intensities = describe_channels(img)
     plt.plot(range(256), r_intensities, 'r')
