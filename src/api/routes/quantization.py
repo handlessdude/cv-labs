26affbcd866d849ea7b19e52cb9558488f7b24ea
heyra <zalomnova.eliz@gmail.com>
2023-10-11 01:15:32 +0300

    feat: convert_to_quantitized

--- src/api/routes/quantization.py
+++ src/api/routes/quantization.py
@@ -5,7 +5,10 @@ from fastapi import Query
 from loguru import logger
 import numpy as np
 from src.pipelines.image_description.methods import describe_channels
-from src.pipelines.quantization.methods import convert_to_halftone
+from src.pipelines.quantization.methods import (
+    convert_to_halftone,
+    convert_to_quantitized,
+)
 from src.models.schemas.image import ImageSchema, ImageHist
 from src.models.schemas.color_correction import CorrectionOut
 from src.utils.fs_io import open_img
@@ -39,6 +42,40 @@ async def halftone(
     )
 
     img_out = convert_to_halftone(img_in)
+    ri_out, gi_out, bi_out = describe_channels(np.asarray(img_out))
+    img_out_schema = ImageSchema(
+        id=uuid4(),
+        img_src=img_to_base64(img_out),
+        hist=ImageHist(r=ri_out, g=gi_out, b=bi_out),
+    )
+    logger.info("Processing image {img_id} done", img_id=img_in_id)
+    return CorrectionOut(img_in=img_in_schema, img_out=img_out_schema)
+
+
+@router.get(
+    path="/quantitize",
+    name="quantization:quantitize",
+    response_model=CorrectionOut,
+    status_code=status.HTTP_200_OK,
+)
+async def quantitize(
+    name: str = Query(description="Image name", default=default_image),
+    levels: str = Query(description="List of quantization levels"),
+):
+    levels = [int(x) for x in levels.split(",")]
+    img_in_id = uuid4()
+    logger.info("Processing quantization for image {img_id}", img_id=img_in_id)
+
+    img_in = open_img(dir_in, name)
+    ri_in, gi_in, bi_in = describe_channels(np.asarray(img_in))
+    img_in_schema = ImageSchema(
+        id=img_in_id,
+        img_src=img_to_base64(img_in),
+        hist=ImageHist(r=ri_in, g=gi_in, b=bi_in),
+    )
+
+    img_out = convert_to_quantitized(img_in, levels)
+    print(img_out)
     ri_out, gi_out, bi_out = describe_channels(np.asarray(img_out))
     img_out_schema = ImageSchema(
         id=uuid4(),
--- src/pipelines/quantization/methods.py
+++ src/pipelines/quantization/methods.py
@@ -1,5 +1,6 @@
 import numpy as np
 from numba import njit, prange
+import cv2
 
 
 halftone_cfs = np.array([0.0721, 0.7154, 0.2125], dtype=np.float32)
@@ -13,4 +14,24 @@ def convert_to_halftone(img_in: np.ndarray):
             img_out[j][i] = np.dot(
                 halftone_cfs.astype(np.float32), img_in[j][i].astype(np.float32)
             )
+
+    # fig, ax = plt.subplots()
+    # ax.imshow(img_out, cmap="gray")
+    # fig.canvas.draw()
+    # pixel_data = np.array(fig.canvas.renderer._renderer)
+    # if pixel_data.shape[2] == 4:
+    #     pixel_data = pixel_data[:, :, 0:3]
     return img_out.astype(np.uint8)
+
+
+def convert_to_quantitized(img_in: np.ndarray, levels: np.ndarray):
+    levels = np.sort(levels)
+    lut = np.zeros(0, dtype=np.uint8)
+    for j in range(len(levels)):
+        lut = np.append(
+            lut, [np.repeat(levels[j], levels[j] - (0 if j == 0 else levels[j - 1]))]
+        )
+    if len(lut) < 256:
+        lut = np.append(lut, [np.repeat(255, 256 - len(lut))])
+    qF = cv2.LUT(img_in, lut)
+    return qF.astype(np.uint8)
