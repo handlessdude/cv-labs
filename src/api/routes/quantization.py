2ef4b2de0895405160fd8726f666dc270d0b3631
heyra <zalomnova.eliz@gmail.com>
2023-10-11 01:48:07 +0300

    feat: otsu global

--- src/api/routes/quantization.py
+++ src/api/routes/quantization.py
@@ -8,6 +8,7 @@ from src.pipelines.image_description.methods import describe_channels
 from src.pipelines.quantization.methods import (
     convert_to_halftone,
     convert_to_quantitized,
+    otsu_global_binarization,
 )
 from src.models.schemas.image import ImageSchema, ImageHist
 from src.models.schemas.color_correction import CorrectionOut
@@ -18,7 +19,7 @@ from src.utils.fs_io import img_to_base64
 router = APIRouter(prefix="/quantization", tags=["quantization"])
 
 dir_in = f"{str(ROOT_DIR)}/app-data/inputs/03"
-default_image = "lisa.png"
+default_image = "gosling1.png"
 
 
 @router.get(
@@ -75,6 +76,37 @@ async def quantitize(
     )
 
     img_out = convert_to_quantitized(img_in, levels)
+    ri_out, gi_out, bi_out = describe_channels(np.asarray(img_out))
+    img_out_schema = ImageSchema(
+        id=uuid4(),
+        img_src=img_to_base64(img_out),
+        hist=ImageHist(r=ri_out, g=gi_out, b=bi_out),
+    )
+    logger.info("Processing image {img_id} done", img_id=img_in_id)
+    return CorrectionOut(img_in=img_in_schema, img_out=img_out_schema)
+
+
+@router.get(
+    path="/otsu-global",
+    name="quantization:otsu-global",
+    response_model=CorrectionOut,
+    status_code=status.HTTP_200_OK,
+)
+async def otsu_global(
+    name: str = Query(description="Image name", default=default_image),
+):
+    img_in_id = uuid4()
+    logger.info("Processing quantization for image {img_id}", img_id=img_in_id)
+
+    img_in = open_img(dir_in, name)
+    ri_in, gi_in, bi_in = describe_channels(np.asarray(img_in))
+    img_in_schema = ImageSchema(
+        id=img_in_id,
+        img_src=img_to_base64(img_in),
+        hist=ImageHist(r=ri_in, g=gi_in, b=bi_in),
+    )
+
+    img_out = otsu_global_binarization(img_in)
     ri_out, gi_out, bi_out = describe_channels(np.asarray(img_out))
     img_out_schema = ImageSchema(
         id=uuid4(),
--- src/pipelines/quantization/methods.py
+++ src/pipelines/quantization/methods.py
@@ -1,7 +1,7 @@
 import numpy as np
 from numba import njit, prange
 import cv2
-
+from src.pipelines.image_description.methods import describe_channels
 
 halftone_cfs = np.array([0.0721, 0.7154, 0.2125], dtype=np.float32)
 
@@ -14,13 +14,6 @@ def convert_to_halftone(img_in: np.ndarray):
             img_out[j][i] = np.dot(
                 halftone_cfs.astype(np.float32), img_in[j][i].astype(np.float32)
             )
-
-    # fig, ax = plt.subplots()
-    # ax.imshow(img_out, cmap="gray")
-    # fig.canvas.draw()
-    # pixel_data = np.array(fig.canvas.renderer._renderer)
-    # if pixel_data.shape[2] == 4:
-    #     pixel_data = pixel_data[:, :, 0:3]
     return img_out.astype(np.uint8)
 
 
@@ -35,3 +28,67 @@ def convert_to_quantitized(img_in: np.ndarray, levels: np.ndarray):
         lut = np.append(lut, [np.repeat(255, 256 - len(lut))])
     qF = cv2.LUT(img_in, lut)
     return qF.astype(np.uint8)
+
+
+def first_of(np_array: np.ndarray, cond):
+    for i in range(np_array.shape[0]):
+        if cond(np_array[i]):
+            return i
+    return None
+
+
+def last_of(np_array, cond):
+    for i in range(np_array.shape[0])[::-1]:
+        if cond(np_array[i]):
+            return i
+    return None
+
+
+def otsuThreshold(img_in, __min=1, __max=255):
+    temp_hist, _, _ = describe_channels(img_in)
+
+    _min = max(__min, first_of(temp_hist, lambda x: x > 0))
+    _max = min(__max, last_of(temp_hist, lambda x: x > 0))
+
+    hist = temp_hist / sum(temp_hist[_min : (_max + 1)])
+
+    m = 0
+    for t in range(_min + 1, _max + 1):
+        m += t * hist[t]
+
+    threshold = _min
+
+    q1 = hist[_min]
+    q2 = 1 - q1
+    mu1 = _min  # * hist[_min] / q1
+    mu2 = m / q2
+
+    maxSigma = q1 * q2 * (mu1 - mu2) * (mu1 - mu2)
+
+    for t in range(_min + 1, _max):
+        mu1 = mu1 * q1 + t * hist[t]
+        mu2 = mu2 * q2 - t * hist[t]
+
+        q1 += hist[t]
+        q2 = 1 - q1
+
+        mu1 = mu1 / q1
+        mu2 = mu2 / q2
+
+        newSigma = q1 * q2 * (mu1 - mu2) * (mu1 - mu2)
+        if maxSigma < newSigma:
+            threshold = t
+            maxSigma = newSigma
+    return threshold
+
+
+def binarization(img_in, t):
+    lut = np.zeros(0, dtype=np.uint8)
+    lut = np.append(lut, [np.repeat(0, t)])
+    lut = np.append(lut, [np.repeat(255, 256 - t)])
+    qF = cv2.LUT(img_in, lut)
+    return qF.astype(np.uint8)
+
+
+def otsu_global_binarization(img_in: np.ndarray):
+    return binarization(img_in, otsuThreshold(img_in))
