f9e8e89fa500a7c3a00c11b174c871e397cf61c2
heyra <zalomnova.eliz@gmail.com>
2023-11-06 22:26:09 +0300

    fix: mapping spectrum to 0-255

--- src/api/routes/frequency_filtering.py
+++ src/api/routes/frequency_filtering.py
@@ -1,22 +1,18 @@
 from fastapi import APIRouter
 from fastapi import status
 from fastapi import Query
-
-from src.models.schemas.base import BaseSchemaModel
 from src.models.schemas.image import ImageSchema
 from src.pipelines.frequency_filtering.core import fft_smoothen_sharpen
-from src.pipelines.spatial_filtering.core import enhance_skeletons
-from src.utils.fs_io import open_img
+from src.utils.fs_io import open_img, make_path
 from src.config.base import ROOT_DIR
-from typing import Sequence
 from src.models.schemas.base import BaseSchemaModel
-from pydantic.types import List, UUID4
 from src.utils.images import get_image_schema
+import cv2 as cv
 
 router = APIRouter(prefix="/frequency-filtering", tags=["frequency-filtering"])
 
 dir_in = f"{str(ROOT_DIR)}/app-data/inputs"
-default_image = "skeleton.jpg"
+default_image = "letters.png"
 
 
 class Test(BaseSchemaModel):
@@ -27,15 +23,24 @@ class Test(BaseSchemaModel):
 @router.get(
     path="/smoothening-sharpening",
     name="frequency-filtering:smoothening-sharpening",
-    response_model=Sequence[ImageSchema],
+    response_model=Test,
     status_code=status.HTTP_200_OK,
 )
 async def smoothen_sharpen(
     name: str = Query(description="Image name", default=default_image),
 ):
-    img_in = open_img(dir_in, name)
+    # img_in = cv.imread(make_path(dir_in, default_image), cv.IMREAD_GRAYSCALE)
+    img_in = cv.cvtColor(
+        cv.imread(make_path(dir_in, default_image), cv.IMREAD_COLOR), cv.COLOR_BGR2GRAY
+    )
+
     pipeline_imgs = fft_smoothen_sharpen(img_in)
+    spectrum = get_image_schema(
+        pipeline_imgs["spectrum"],
+        "spectrum.png",
+        include_hist=False,
+    )
     return Test(
-        source=get_image_schema(img_in, name),
-        spectrum=get_image_schema(pipeline_imgs, "spectrum"),
+        source=get_image_schema(img_in, name, include_hist=False),
+        spectrum=spectrum,
     )
--- src/pipelines/frequency_filtering/core.py
+++ src/pipelines/frequency_filtering/core.py
@@ -6,8 +6,15 @@ def get_magnitude_spectrum(img_in: np.ndarray):
     f = np.fft.fft2(img_in)
     fshift = np.fft.fftshift(f)
     magnitude_spectrum = np.log(np.abs(fshift))
+    print(magnitude_spectrum)
     return magnitude_spectrum
 
 
+def shit_to_ok(img_in: np.ndarray):
+    img_in_normalized = (img_in - np.min(img_in)) / (np.max(img_in) - np.min(img_in))
+    return (img_in_normalized * 255).astype(np.uint8)
+
+
 def fft_smoothen_sharpen(img_in: np.ndarray):
-    return {"spectrum": get_magnitude_spectrum(img_in)}
+    spectrum = get_magnitude_spectrum(img_in)
+    return {"spectrum": shit_to_ok(spectrum)}
