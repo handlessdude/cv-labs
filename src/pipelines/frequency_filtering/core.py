1d03332fb62e95f9a7dbf04203169f8b12d9d022
heyra <zalomnova.eliz@gmail.com>
2023-11-08 00:28:04 +0300

    refactor: rearrange frequency filtering pipeline

--- src/pipelines/frequency_filtering/core.py
+++ src/pipelines/frequency_filtering/core.py
@@ -3,39 +3,11 @@ import numpy as np
 from src.pipelines.frequency_filtering.kernels import get_ideal_filter
 from src.pipelines.frequency_filtering.methods import (
     normalize_to_uint8,
+    apply_filter,
 )
 
-EPS = 1e-10
+
 IDEAL_FILTER_R = 30
-
-
-def replace_zeros(img_in: np.ndarray):
-    return np.where(img_in == 0, EPS, img_in)
-
-
-def log_of_abs(arr_in: np.ndarray):
-    return np.log(np.abs(arr_in))
-
-
-def get_spectrum(img_in: np.ndarray):
-    base_img = replace_zeros(img_in.astype(np.float32))
-    dft = np.fft.fft2(base_img)
-    dft_shift = np.fft.fftshift(dft)  # here applies filter
-    spectrum = log_of_abs(dft_shift)
-    return dft_shift, spectrum
-
-
-def apply_filter(img_in: np.ndarray, filter: np.ndarray):
-    dft_shift, img_in_spectrum = get_spectrum(img_in)
-
-    filter_applied = dft_shift * filter
-
-    dft_ishift = np.fft.ifftshift(filter_applied)
-    idft = np.fft.ifft2(dft_ishift)
-    img_out = np.abs(idft)
-    _, img_out_spectrum = get_spectrum(img_out)
-
-    return img_in_spectrum, img_out_spectrum, img_out
 
 
 def apply_ideal_filter(img_in: np.ndarray):
--- src/pipelines/frequency_filtering/methods.py
+++ src/pipelines/frequency_filtering/methods.py
@@ -8,3 +8,35 @@ def normalize_to_uint8(img_in: np.ndarray):
 
 def vec_len(first: np.ndarray, second: np.ndarray):
     return np.linalg.norm(first - second)
+
+
+EPS = 1e-10
+
+
+def replace_zeros(img_in: np.ndarray):
+    return np.where(img_in == 0, EPS, img_in)
+
+
+def log_of_abs(arr_in: np.ndarray):
+    return np.log(np.abs(arr_in))
+
+
+def get_spectrum(img_in: np.ndarray):
+    base_img = replace_zeros(img_in.astype(np.float32))
+    dft = np.fft.fft2(base_img)
+    dft_shift = np.fft.fftshift(dft)  # here applies filter
+    spectrum = log_of_abs(dft_shift)
+    return dft_shift, spectrum
+
+
+def apply_filter(img_in: np.ndarray, filter: np.ndarray):
+    dft_shift, img_in_spectrum = get_spectrum(img_in)
+
+    filter_applied = dft_shift * filter
+
+    dft_ishift = np.fft.ifftshift(filter_applied)
+    idft = np.fft.ifft2(dft_ishift)
+    img_out = np.abs(idft)
+    _, img_out_spectrum = get_spectrum(img_out)
+
+    return img_in_spectrum, img_out_spectrum, img_out
